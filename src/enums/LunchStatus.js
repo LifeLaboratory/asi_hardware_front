560e0b838608e519f7e02c4d2dc23b28875af535
Laloww <lalow.main@gmail.com>
2019-08-25 01:00:03 +0700

    merge

--- src/enums/LunchStatus.js
+++
@@ -1,8 +0,0 @@
-const LaunchStatus = {
-        "pending":"pending",
-        "approved":"approved",
-        "declined": "declined",
-        "finished": "finished"
-}
-
-export default LaunchStatus
